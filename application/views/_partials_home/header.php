03fdc3cb85691fe6a6d0aff00e26fb818b7a6370
Budi Gunawan <gunawanbudi930@gmail.com>
2020-01-27 13:19:03 +0700

    Base web

---
+++ application/views/_partials_home/header.php
@@ -0,0 +1,34 @@
+<!-- ========== Meta Tags ========== -->
+<meta charset="UTF-8">
+<meta name="description" content="Evento -Event Html Template">
+<meta name="keywords" content="Evento , Event , Html, Template">
+<meta name="author" content="ColorLib">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!-- ========== Title ========== -->
+<title> Evento - Event Html Template</title>
+<!-- ========== Favicon Ico ========== -->
+<!--<link rel="icon" href="fav.ico">-->
+<!-- ========== STYLESHEETS ========== -->
+<!-- Bootstrap CSS -->
+<link href="<?php echo base_url() ?>assets/evento/assets/css/bootstrap.min.css" rel="stylesheet">
+<!-- jquery -->
+<script src="<?php echo base_url() ?>assets/evento/assets/js/jquery.min.js"></script>
+<!-- Fonts Icon CSS -->
+<link href="<?php echo base_url() ?>assets/evento/assets/css/font-awesome.min.css" rel="stylesheet">
+<link href="<?php echo base_url() ?>assets/evento/assets/css/et-line.css" rel="stylesheet">
+<link href="<?php echo base_url() ?>assets/evento/assets/css/ionicons.min.css" rel="stylesheet">
+<!-- Carousel CSS -->
+<link href="<?php echo base_url() ?>assets/evento/assets/css/owl.carousel.min.css" rel="stylesheet">
+<link href="<?php echo base_url() ?>assets/evento/assets/css/owl.theme.default.min.css" rel="stylesheet">
+<!-- Animate CSS -->
+<link rel="stylesheet" href="<?php echo base_url() ?>assets/evento/assets/css/animate.min.css">
+<!-- Custom styles for this template -->
+<link href="<?php echo base_url() ?>assets/evento/assets/css/main.css" rel="stylesheet">
+<script type="text/javascript" src="<?php echo base_url() ?>assets/smint/js/jquery.smint.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('.subMenu').smint({
+			'scrollSpeed': 1000
+		});
+	});
+</script>
---
+++ application/views/_partials_home/menu.php
@@ -0,0 +1,36 @@
+<!--header start here -->
+<header class="header navbar fixed-top navbar-expand-md">
+    <div class="container">
+        <a class="navbar-brand logo" href="#">
+            <img src="<?php echo base_url() ?>assets/evento/assets/img/logo.png" alt="Evento"/>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#headernav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="lnr lnr-text-align-right"></span>
+        </button>
+        <div class="collapse navbar-collapse flex-sm-row-reverse" id="headernav">
+            <ul class="nav navbar-nav menu">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#s1">Speakers</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#s2">Events</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#s3">News</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#s4">Contact</a>
+                </li>
+                <li class="search_btn">
+                    <a  href="#">
+                       <i class="ion-ios-search"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
+<!--header end here-->
